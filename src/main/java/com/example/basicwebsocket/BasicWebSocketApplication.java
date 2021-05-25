1a71ca1488a4e5421d748676b7611fe5a83fdbe0
yurim022 <yurim2220@gmail.com>
2021-05-25 23:52:45 +0900

    feat(WebSocketHandler) server & client connect using spring-web-socket

---
+++ src/main/java/com/example/basicwebsocket/BasicWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicWebSocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/basicwebsocket/BasicWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
